cd8abd20c451475388457c74acfbe04770345251
samitariq12 <samitariq40@gmail.com>
2020-10-12 02:47:08 -0700

    travis updated

---
+++ build/precache-manifest.2141b55c03eaf9cc7cae8cd9785ab8a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02be59307a15d01d8dd4d1db109d3139",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a0d10fcdf8de4b9ef61",
+    "url": "/static/css/main.b38eb0f9.chunk.css"
+  },
+  {
+    "revision": "fd63111a8452883eff97",
+    "url": "/static/js/2.f7fcd272.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f7fcd272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a0d10fcdf8de4b9ef61",
+    "url": "/static/js/main.da1b8f9e.chunk.js"
+  },
+  {
+    "revision": "15f4b3d0bcecc9afea01",
+    "url": "/static/js/runtime-main.e03fec8a.js"
+  },
+  {
+    "revision": "63f874d192fb3892d88d5e26f942b5e2",
+    "url": "/static/media/DSDIGI.63f874d1.ttf"
+  },
+  {
+    "revision": "a902ed5c545e59d5d0fd6c5f42817c66",
+    "url": "/static/media/another.a902ed5c.jpg"
+  }
+]);
